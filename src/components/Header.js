063f20981239dc0b555c36ee657d5e5a76378341
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2024-07-21 01:24:02 -0400

    message

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import pdfResume from "../image/Resume(Sara).pdf";
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
+
+
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className="header">
             <h1><Link to="/">Sara Ettehad</Link></h1>
-            <nav>
+            <nav  className={isOpen ? 'nav-open' : ''}>
                 <ul>
                     <li><Link to="/about">About Me</Link></li>
                     <li><a href="/#projects1">Coding Projects</a></li>
@@ -14,6 +23,11 @@ const Header = () => {
                     <li><a href= { pdfResume } target="_blank" rel="noopener noreferrer">Resume</a></li>
                 </ul>
             </nav>
+            <div className="hamburger" onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            </div>
         </header>
     );
 };
